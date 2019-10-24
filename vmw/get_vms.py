a0945061fb91ab6be84ed792a51db25b04606320
Lokeshnk09 <loks.nk@gmail.com>
2019-10-24 16:28:30 -0700

    vsphere.datacenter

--- vmw/get_vms.py
+++ vmw/get_vms.py
@@ -1,8 +1,42 @@
 from pyVmomi import vim
-from vmw import vc_connect
+from vmw.vc_connect import *
 from pyVim.connect import SmartConnect, Disconnect
 
 
-def get_vm_list():
-    datacenter = c.content.rootFolder()
+clusters = get_all_obj(content, [vim.ClusterComputeResource])
+for cluster in clusters:
+    print("ClusterName:", cluster.name)
 
+
+def get_vms():
+    count = 0
+    vms = get_all_obj(content, [vim.VirtualMachine])
+    _virtual_machines = {}
+    for vm in vms:
+        print(count, vm.name)
+        count += 1
+    return vms
+
+
+v = get_vms()
+
+
+''''
+        _ip_address = ""
+        summary = vm.summary
+        if summary.guest is not None:
+            _ip_address = summary.guest.ipAddress
+            if _ip_address is None:
+                _ip_address = ""
+
+        virtual_machine = {
+            summary.config.name: {
+                "guest_fullname": summary.config.guestFullName,
+                "power_state": summary.runtime.powerState,
+                "ip_address": _ip_address
+            }
+        }
+
+        _virtual_machines.update(virtual_machine)
+    return _virtual_machines 
+'''
