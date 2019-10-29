c65e2cf306f0df8695023d2c3cda05106fa02e6d
Lokeshnk09 <loks.nk@gmail.com>
2019-10-28 17:01:22 -0700

    vsphere.datacenter

---
+++ class/methods.py
@@ -0,0 +1,32 @@
+
+class Student():
+
+    School = 'SVS'
+
+    def __init__(self, m1,m2,m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+
+    @classmethod
+    def get_school_info(cls):
+        return cls.School
+
+    @staticmethod
+    def info():
+        print('Welcome to SVS School')
+
+
+s1 = Student(52,65,98)
+s2 = Student(45,98,79)
+
+av1 = Student.avg(s1)
+av2 = Student.avg(s2)
+
+print(av1)
+print(av2)
+print(Student.get_school_info())
+print(Student.info())
