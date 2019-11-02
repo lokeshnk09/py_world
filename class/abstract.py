1e625b5c4e7f66b1517c68cfc16fc3f4d2861aad
lokeshnk09 <loks.nk@gmail.com>
2019-11-01 18:29:30 -0700

    Anjan Traninig

--- class/abstract.py
+++ class/abstract.py
@@ -5,34 +5,31 @@ import datetime
 import re
 
 #cur_year = (calendar.TextCalendar(calendar.SUNDAY).formatyear(2019, 2, 1, 1, 2))
-help(datetime)
-now = datetime.datetime.now()
-prev = datetime.date(2018, 12, 1)
-print(now.strftime("%m-%d-%Y %I:%M"))
-print(prev)
+#help(datetime)
+#now = datetime.datetime.now()
+#prev = datetime.date(2018, 12, 31)
+
 
 #pre_year = (calendar.TextCalendar(calendar.SUNDAY).formatyear(2018, 2, 1, 1, 2))
 
 class Vehicle:
 
-    def __init__(self, ct, pt):
-        self.ct = ct
-        self.pt = pt
+    def __init__(self, car, truck):
+        self.car = car
+        self.truck = truck
 
-    def cur_time(self):
-        self.ct = datetime.datetime.now()
-        return now.strftime("%m-%d-%Y %I:%M")
+    def make(self):
+        return input('Enter make:')
 
-    def pre_time(self):
-        self.pt = datetime.date(2018, 12, 31)
-        return
+    def model(self):
+        return int(input('Enter model:'))
 
-    def __lt__(self):
-        return
 
 
 class Dmv(Vehicle):
-    def lic_plate(self):
+    def __init__(self, ct, pt, lic_plate):
+        self.ct = ct
+        self.pt = pt
         self.lic_plate = lic_plate(input('Enter Lic Plate No:'))
         m = re.fullmatch('[0-9][A-Z]{3}\d{3}', lic_plate)
         if m!= None:
@@ -40,14 +37,25 @@ class Dmv(Vehicle):
         else:
             print('Invalid number')
 
+    def cur_time(self):
+        self.ct = datetime.datetime.now()
+        return self.ct.strftime("%m-%d-%Y %I:%M")
+
+    def pre_time(self):
+        self.pt = datetime.date(2018, 12, 31)
+        return self.pt
+
+    def __lt__(self, date):
+        print(self.pt - date)
+
     def set_lic(self):
         pass
 
     def get_lic(self):
-        return self.vehicle
+        pass
 
 
-car = Vehicle.set_lic(2019)
+
 
 
 
