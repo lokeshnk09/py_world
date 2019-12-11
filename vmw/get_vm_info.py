5bb51e44429ed5a6e10d71ed5240d91acbc9ab8b
Lokeshnk09 <loks.nk@gmail.com>
2019-12-11 10:45:03 -0800

    Office_Desktop!

--- vmw/get_vm_info.py
+++ vmw/get_vm_info.py
@@ -1,6 +1,8 @@
 from pyVim import connect
 import ssl
 from pyVmomi import vim
+from pyVmomi import vmodl
+
 
 s = ssl.SSLContext(ssl.PROTOCOL_SSLv23)  # For VC 6.5/6.0 s=ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 s.verify_mode = ssl.CERT_NONE
@@ -25,7 +27,7 @@ def get_all_obj(content, viewtype):
 
 
 def get_vms_info():
-    count = 0
+
     vms = get_all_obj(sc, [vim.VirtualMachine])
     for vm in vms:
         summary = vm.summary
@@ -42,7 +44,6 @@ def get_vms_info():
             else:
                 print("IP         : None")
         print('--------------------------------')
-        count += 1
 
 
 if __name__ == "__main__":
