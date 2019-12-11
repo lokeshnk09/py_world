6de8c4f990f5639b2a8ed8b025cfdb250560b360
lokeshnk09 <loks.nk@gmail.com>
2019-12-11 10:20:25 -0800

    Merge branch 'master' of https://github.com/lokeshnk09/py_world
     Conflicts:
    	Func/f2.py
    	class/abstract.py

---
+++ project2/calc_func.py
@@ -0,0 +1,28 @@
+
+import math
+
+
+class My_Calculator:
+
+    def func_add(self, a,b):
+        return a + b
+
+    def func_mul(self, a,b):
+        return a * b
+
+
+if __name__ == '__main__':
+    ad = My_Calculator()
+    print(ad.func_add(3, 4))
+    print(ad.func_mul(4, 4))
+
+
+
+
+
+
+
+
+
+
+
